e316864920f578febb03f796bf0e80c8dfaa8f9c
naimurnemu <naimurrahaman82@gmail.com>
2021-08-17 20:05:21 +0600

    interactive headin button

---
+++ js/event.js
@@ -0,0 +1,4 @@
+document.getElementById("header-btn").addEventListener("click", function () {
+    const caption = document.getElementById("header-caption");
+    caption.innerText = "I AM PENGUIN":
+});
