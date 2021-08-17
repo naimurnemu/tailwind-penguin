66482a51d2c956f964fe2cfe6a80b3f11d7325cd
naimurnemu <naimurrahaman82@gmail.com>
2021-08-17 20:06:26 +0600

    resolve

--- js/event.js
+++ js/event.js
@@ -1,4 +1,4 @@
 document.getElementById("header-btn").addEventListener("click", function () {
     const caption = document.getElementById("header-caption");
-    caption.innerText = "I AM PENGUIN":
+    caption.innerText = "I AM PENGUIN";
 });
